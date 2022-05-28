a59954f1d60a4b398d2750de4e01867818625b7b
Picachune <101989919+Picachune@users.noreply.github.com>
2022-05-28 08:41:32 +0600

    check-1

--- git1.py
+++ git1.py
@@ -1,1 +1,15 @@
 print("again start to fress")
+
+#age calculator
+
+# frome birthday
+
+from datetime import date
+def calculator(birthday):
+    today = date.today()
+    age = today.year - birthday.year - ((today.month,today.day)< (birthday.month,birthday.day))
+    return age
+
+print('your age is : ',calculator(date(2001,4,15)))
+#chenges things for new branch
+print('you are welcome')
